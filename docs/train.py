72109475e47c1fe55d5de5265f42647fa45f21ee
Max-1em <142114697+Max-1em@users.noreply.github.com>
2026-03-13 10:34:04 +0100

    Add model training script for Iris dataset

---
+++ docs/train.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+data = load_iris()
+
+X = data.data
+y = data.target
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, "model/model.pkl")
+
+print("Model training completed")
